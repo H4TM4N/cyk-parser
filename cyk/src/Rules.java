530139df45e7a38274ec5a41fde9f3c38632c469
Ryo Hatakenaka <hatman06050@gmail.com>
2020-05-22 21:44:59 +0900

    Update Rules.java

--- cyk/src/Rules.java
+++ cyk/src/Rules.java
@@ -7,8 +7,7 @@ import java.util.*;
 *<p>Usage:
 *	First: call setRuleMap(String) in main method
 *	Then : use getRule method if you want to check
-*
-*@author Ryo Hatakenaka
+*</p>
 *
 */
 public class Rules{
